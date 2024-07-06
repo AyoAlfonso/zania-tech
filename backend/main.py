3083c4331a8057c4d34dce24e956bffc94a5c4f8
Ayo Alfonso <alfonsoayo7@gmail.com>
2024-07-06 15:44:40 +0100

    Add new

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,10 @@ app = FastAPI()
 Document_Pydantic = pydantic_model_creator(Document, name="Document")
 DocumentIn_Pydantic = pydantic_model_creator(Document, name="DocumentIn", exclude_readonly=True)
 
+@app.get("/")
+async def read_root():
+    return {"status": 200, "message": "welcome !"}
+
 @app.post("/documents", response_model=Document_Pydantic)
 async def create_document(document: DocumentIn_Pydantic):
     obj = await Document.create(**document.dict())
@@ -24,10 +28,11 @@ async def get_document(document_id: int):
 async def list_documents():
     return await Document_Pydantic.from_queryset(Document.all())
 
+
 register_tortoise(
     app,
     db_url='sqlite://db.sqlite3',
-    modules={'models': ['models']},
+    modules={'model': ['model']},
     generate_schemas=True,
     add_exception_handlers=True,
 )
